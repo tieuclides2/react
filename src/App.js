9cbce39dc2e8b906f7010b7eb5425708542b109b
Gabriel <gabriel-euclides@hotmail.com>
2023-12-06 07:09:16 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Word React</h1>
+      <div className='myname'>
+        <h2>Meu nome eh Gabriel!</h2>
+      </div>
+      <p>Meu primeiro paragrafo no react.</p>
+      
+    </div>
+  );
+}
+
+export default App;
